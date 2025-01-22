d49a40c8c5dd6f98b13f9eff6167919f77709a24
jamesmica <123773695+jamesmica@users.noreply.github.com>
2025-01-22 12:09:48 +0100

    Update script1.js

--- script1.js
+++ script1.js
@@ -8,8 +8,8 @@ var geojsonLayer;
 var correspondance;
 
 var map = L.map('map').setView([46.71109, 1.7191036], 6);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© OpenStreetMap contributors'
+L.tileLayer('//{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+    attribution: 'donn&eacute;es &copy; <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>'
 }).addTo(map);
 
 var rowsGSheet; // Données Google Sheets
